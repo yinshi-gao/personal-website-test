70d3f073f63971c3333a30d5425b624e9f97f82e
Yinshi Gao <gaoyinshi415@gmail.com>
2022-10-27 11:29:47 -0400

    byebye about

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ReactMarkdown from 'react-markdown';
+import raw from 'raw.macro';
 
 import Main from '../layouts/Main';
+
+// uses babel to load contents of file
+const markdown = raw('../data/about.md');
 
 const Index = () => (
   <Main
@@ -10,17 +15,38 @@ const Index = () => (
     <article className="post" id="index">
       <header>
         <div className="title">
-          <h2 data-testid="heading"><Link to="/">About this site</Link></h2>
+          <h2 data-testid="heading"><Link to="/">About me</Link></h2>
         </div>
       </header>
-      <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
-        or you can check out my {' '}
-        <Link to="/research">research</Link>, {' '}
-        <Link to="/teaching">teaching</Link>, {' '}
-        or <Link to="/contact">contact</Link> me.
-      </p>
+      <ReactMarkdown
+        source={markdown}
+//        renderers={{
+//          Link: LinkRenderer,
+//        }}
+      />
     </article>
   </Main>
-);
+  );
+
+// const Index = () => (
+//   <Main
+//     description={"Yinshi Gao's personal website."}
+//   >
+//     <article className="post" id="index">
+//       <header>
+//         <div className="title">
+//           <h2 data-testid="heading"><Link to="/">About this site</Link></h2>
+//         </div>
+//       </header>
+//       <p> Welcome to my website.
+//         Please feel free to read more <Link to="/about">about me</Link>,
+//         or you can check out my {' '}
+//         <Link to="/research">research</Link>, {' '}
+//         <Link to="/teaching">teaching</Link>, {' '}
+//         or <Link to="/contact">contact</Link> me.
+//       </p>
+//     </article>
+//   </Main>
+// );
 
 export default Index;
